b5f168595ed378c7dc360a38256c8e3409c1ebc2
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-10-17 19:51:00 +0200

    [UPD] Added type and technologies into the cards

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -10,8 +10,17 @@ export default {
     <div class="col-12 col-md-6 col-lg-4">
         <div class="card">
           <img class="card-img-top" :src="project.image != null ? `http://127.0.0.1:8000/storage/${project.image}` : 'https://placehold.co/300x200?text=imagine+copertina' " alt="">
-          <div class="card-body">
+          <div class="card-body bg-dark text-white">
             <h4 class="card-title text-center">{{ project.name }}</h4>
+            <p><strong class="text-warning">{{ project.type ? project.type.name : 'Tipologia non definita'}}</strong></p>
+            <ul class="list-unstyled d-flex text-info" v-if="project.technologies">
+                <li class="me-1" v-for="(tech, index) in project.technologies.slice(0, -1)">
+                  {{ tech.name }},
+                </li>
+                <li v-for="tech in project.technologies.slice(-1)">
+                  {{ tech.name }}
+                </li>
+              </ul>
             <p>{{ project.summary }}</p>
           </div>
         </div>
