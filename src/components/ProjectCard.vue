52d7aa6493025870c02cab50a553a20035f39782
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-10-18 00:17:25 +0200

    [UPD] Created pagination

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -3,6 +3,11 @@ export default {
     name: 'ProjectCard',
     props:{
         project: Object
+    }, 
+    methods:{
+        truncateText(text){
+            return text != null ? text.substr(0, 100) + '...' : '';
+        }
     }
 }
 </script>
@@ -11,17 +16,17 @@ export default {
         <div class="card">
           <img class="card-img-top" :src="project.image != null ? `http://127.0.0.1:8000/storage/${project.image}` : 'https://placehold.co/300x200?text=imagine+copertina' " alt="">
           <div class="card-body bg-dark text-white">
-            <h4 class="card-title text-center">{{ project.name }}</h4>
-            <p><strong class="text-warning">{{ project.type ? project.type.name : 'Tipologia non definita'}}</strong></p>
-            <ul class="list-unstyled d-flex text-info" v-if="project.technologies">
-                <li class="me-1" v-for="(tech, index) in project.technologies.slice(0, -1)">
+            <h4 class="card-title text-center text-decoration-underline">{{ project.name }}</h4>
+            <p class="mt-4"><strong class="text-warning">{{ project.type ? project.type.name : 'Tipologia non definita'}}</strong></p>
+            <ul class="list-unstyled d-flex text-info" v-if="project.technologies.length > 0">
+                <li class="me-1" v-for="tech in project.technologies.slice(0, -1)">
                   {{ tech.name }},
                 </li>
                 <li v-for="tech in project.technologies.slice(-1)">
                   {{ tech.name }}
                 </li>
-              </ul>
-            <p>{{ project.summary }}</p>
+            </ul>
+            <p>{{ truncateText(project.summary) }}</p>
           </div>
         </div>
       </div>
